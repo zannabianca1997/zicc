4bd01bad27c72ba29266f5726a38fa9bb9e30303
zannabianca1997 <zannabianca199712@gmail.com>
2023-07-02 17:44:33 +0200

    removed compound errors

--- assembler/src/lexer/mod.rs
+++ assembler/src/lexer/mod.rs
@@ -10,53 +10,11 @@ pub use token::*;
 #[derive(Debug,Error)]
 pub enum Error {
     UnknownToken(Span),
-    Multiple(Vec<Error>),
-}
-impl Error {
-    #[must_use]
-    #[inline]
-    fn is_empty(&self)->bool {
-        matches!(self, Self::Multiple(errs) if errs.is_empty())
-    }
 }
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Error::UnknownToken(pos) => write!(f, "Unknow token at {pos:?}"),
-            Error::Multiple(errs) => {
-                if errs.is_empty() {
-                    panic!("Empty error should never reach the user, so a Display implementation shoul not be provided")
-                }
-                if f.alternate() {
-                    writeln!(f, "Found {} lexing errors:", errs.len())?;
-                    for err in errs {
-                        writeln!(f, "{err}")?
-                    }
-                } else {
-                    write!(f, "Found {} lexing errors: ", errs.len())?;
-                    for err in errs {
-                        write!(f, "{err}; ")?
-                    }
-                }
-                Ok(())
-            },
-        }
-    }
-}
-impl Default for Error {
-    fn default() -> Self {
-        Self::Multiple(vec![])
-    }
-}
-impl Extend<Error> for Error {
-    fn extend<T: IntoIterator<Item = Error>>(&mut self, iter: T) {
-        if let Self::Multiple(errs) = self  {
-            errs.extend(iter)
-        } else {
-            let e1 = mem::take(self);
-            let Self::Multiple(errs) = self else {unreachable!()};
-            errs.push(e1);
-            errs.extend(iter)
         }
     }
 }
@@ -101,7 +59,7 @@ mod tests {
     #[test]
     fn empty() {
         let source = "";
-        let (tokens, errors): (Vec<_>, Error) = lex(source).partition_result();
+        let (tokens, errors): (Vec<_>, Vec<_>) = lex(source).partition_result();
         assert!(tokens.is_empty()&&errors.is_empty())
     }
 
