980da90ef8275d6ad33011346e50736ed67d7c54
zannabianca1997 <zannabianca199712@gmail.com>
2026-04-02 22:16:49 +0200

    Feat(compiler/ast): Added the punctuators and keywords to the tokens

--- compiler/ast/src/tokens.rs
+++ compiler/ast/src/tokens.rs
@@ -1,2 +1,18 @@
+use keywords::logos_keywords;
+use logos::Logos;
+use macro_rules_attribute::apply;
+use punctuators::logos_punctuators;
+
+use crate::tokens::identifiers::Identifier;
+
 pub mod identifiers;
+pub mod keywords;
 pub mod punctuators;
+
+/// A token of a zicc source
+#[apply(logos_punctuators)] // Add a `Punctuator` variant
+#[apply(logos_keywords)] // Add a `Keyword` variant
+#[derive(Logos)]
+pub enum Token {
+    Identifier(Identifier),
+}
