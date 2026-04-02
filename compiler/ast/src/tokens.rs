da0baddf001b0573a06ea83fd40597369c8afad7
zannabianca1997 <zannabianca199712@gmail.com>
2026-04-02 22:26:42 +0200

    Feat(compiler/ast): Added identifier regex

--- compiler/ast/src/tokens.rs
+++ compiler/ast/src/tokens.rs
@@ -2,6 +2,7 @@ use keywords::logos_keywords;
 use logos::Logos;
 use macro_rules_attribute::apply;
 use punctuators::logos_punctuators;
+use string_interner::DefaultStringInterner;
 
 use crate::tokens::identifiers::Identifier;
 
@@ -13,6 +14,12 @@ pub mod punctuators;
 #[apply(logos_punctuators)] // Add a `Punctuator` variant
 #[apply(logos_keywords)] // Add a `Keyword` variant
 #[derive(Logos)]
+#[logos(extras=LexerExtras)]
 pub enum Token {
+    #[regex(r#"[\w&&[^\d_]]\w*|_+[\w&&[^_]]\w*"#, |lex| Identifier::new(lex.slice(), &mut lex.extras.interner))]
     Identifier(Identifier),
+}
+
+pub struct LexerExtras {
+    pub interner: DefaultStringInterner,
 }
